ee7438d72c95e3194b973239074d4bc561c0f153
Riccardo Pittau <elfosardo@gmail.com>
2021-01-11 18:26:17 +0100

    Add correct babele config
    Also update manifest path

---
+++ it-IT/babele-register.js
@@ -0,0 +1,10 @@
+Hooks.once('init', () => {
+
+	if(typeof Babele !== 'undefined') {
+		Babele.get().register({
+			module: 'FoundryVTT-dnd35e-it',
+			lang: 'it',
+			dir: 'compendium'
+        });
+    }
+});
