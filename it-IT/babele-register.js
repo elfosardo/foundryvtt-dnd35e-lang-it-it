09fa79b7f66e0ce9d93f9ff5cd93f182416d2e1b
luca santalucia <luca.santalucia@gmail.com>
2021-06-26 20:08:48 +0200

    fix race condition su cache sistema, e alcune traduzioni mancanti sul nome della classe per associazione abilità

--- it-IT/babele-register.js
+++ it-IT/babele-register.js
@@ -1,12 +1,100 @@
+import * as cache from "../../systems/D35E/module/cache.js";
+import { CACHE } from "../../systems/D35E/module/cache.js";
 
-Hooks.once('init', () => {
+function classTransformation(cl) {
+  switch (cl) {
+    case "Druid":
+      return "Druido";
+    case "Barbarian":
+      return "Barbaro";
+    case "Cleric":
+      return "Chierico";
+    case "Fighter":
+      return "Guerriero";
+    case "Monk":
+      return "Monaco";
+    case "Paladin":
+      return "Paladino";
+    case "Rogue":
+      return "Ladro";
+    case "Sorcerer":
+      return "Stregone";
+    case "Wizard":
+      return "Mago";
+    case "Bard":
+      return "Bardo";
+    case "Aristocrat (NPC)":
+      return "Aristocratico (PNG)";
+    case "Commoner (NPC)":
+      return "Popolano (PNG)";
+    case "Warrior (NPC)":
+      return "Combattente (PNG)";
+    case "Psychic Warrior":
+      return "Psychic Warrior";
+    case "Blackguard":
+      return "Guardia Nera";
+    case "Expert (NPC)":
+      return "Esperto (PNG)";
+    case "Adept (NPC)":
+      return "Adepto (PNG)";
+    case "Soulknife":
+      return "Soulknife";
+    case "Wizard Familiar":
+      return "Famiglio";
+    case "Animal Companion":
+      return "Compagno Animale";
+    case "Ranger":
+      return "Ranger";
+    case "Assassin":
+      return "Assassino";
+    case "Arcane Trickster":
+      return "Arcane Trickster";
+    case "Dwarven Defender":
+      return "Difensore Nanico";
+    default:
+      return cl;
+  }
+}
 
-	if(typeof Babele !== 'undefined') {
-		Babele.get().register({
-			module: 'FoundryVTT-dnd35e-it',
-			lang: 'it',
-			dir: 'compendium'
-		});
-	}
+async function fixSystemCache() {
+  console.error("fixSystemCache");
+  await cache.buildCache();
+  console.log("IT-TRANSLATION | Cache is ", CACHE);
+  const cacheReady = !Object.keys(CACHE)
+    .map((k) => CACHE[k])
+    .some((map) => {
+      if (map.size !== undefined) {
+        return map.size === 0;
+      }
+      return map.lenght === 0;
+    });
 
+  if (cacheReady) {
+    return;
+  }
+  setTimeout(fixSystemCache, 5000);
+}
+
+Hooks.once("init", () => {
+  if (typeof Babele !== "undefined") {
+    Babele.get().register({
+      module: "FoundryVTT-dnd35e-it",
+      lang: "it",
+      dir: "compendium",
+    });
+
+    Babele.get().registerConverters({
+      classNames: (associations) => {
+        if (associations.classes) {
+          associations.classes.map((association) => {
+            association[0] = classTransformation(association[0]);
+            return association;
+          });
+        }
+        return associations;
+      },
+    });
+
+    setTimeout(fixSystemCache, 5000);
+  }
 });
